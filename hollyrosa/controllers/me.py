70141859f8f3f3418a93e24b44fc14563101ce1c
Martin Eliasson <asplunden@gmail.com>
2012-11-03 12:24:15 +0100

    * added missing controler file  * changed cookie so use left click to open context menus works everywhere  * added left click cookie usage everywhere  * menu cleanup  * fixed tg.url in view_activity

---
+++ hollyrosa/controllers/me.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2010, 2011, 2012 Martin Eliasson
+
+This file is part of Hollyrosa
+
+Hollyrosa is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hollyrosa is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Hollyrosa.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import pylons
+from tg import expose, flash, require, url, request, redirect,  validate
+from repoze.what.predicates import Any, is_user, has_permission
+from hollyrosa.lib.base import BaseController
+from hollyrosa.model import holly_couch
+
+import datetime
+
+#...this can later be moved to the VisitingGroup module whenever it is broken out
+from hollyrosa.controllers.common import has_level, DataContainer, getLoggedInUserId
+
+from hollyrosa.model.booking_couch import genUID 
+from hollyrosa.controllers.booking_history import remember_tag_change
+from formencode import validators
+
+__all__ = ['me']
+
+
+class Me(BaseController):
+    def view(self, url):
+        """Abort the request with a 404 HTTP status code."""
+        abort(404)    
+        
+    
+    @expose('hollyrosa.templates.me')
+    @require(Any(is_user('user.erspl'), has_level('staff'), has_level('view'), msg='Only logged in users may view me properties'))
+    def settings(self):
+        return dict()
