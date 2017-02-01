53b4b98787e5dbb480716b45758d8e36a5d0b299
Martin Eliasson <asplunden@gmail.com>
2017-02-01 22:24:18 +0100

    * added js files while refactoring visting group view all page moving js to separate files

---
+++ hollyrosa/public/scripts/page/tag_and_note.js
@@ -0,0 +1,108 @@
+/**
+* Copyright 2010-2017 Martin Eliasson
+*
+* This file is part of Hollyrosa
+*
+* Hollyrosa is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Hollyrosa is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Hollyrosa.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/**
+Modules that can be loaded:
+
+**/
+define(["tags", "dojo/dom-attr", "dojo/_base/array", "dojo/dom-construct", "dojo/_base/window", "dijit/Menu","dijit/MenuItem", "dijit/CheckedMenuItem", "dijit/MenuSeparator", "dojo/query", "dojo/on", "dojo/io-query", "dojo/json", "dojo/cookie", "dojo/dom-style", "dojo/domReady!"],
+  function(tags, domAttr, array, domConstruct, win, Menu, MenuItem, CheckedMenuItem, MenuSeparator, query, on, ioQuery, json, cookie, domStyle)
+  {
+    function updateNotes(data, tag_and_note_config) {
+      var vgroup_id = data['id'];
+      var load_note_link = dom.byId('load_notes_for_' + vgroup_id);
+      domConstruct.destroy(load_note_link);
+
+      var notes = data['notes'];
+      for (n in notes) {
+        note = notes[n];
+        var notes_for_id = 'notes_for_' + note['target_id'];
+
+        e = dom.byId(notes_for_id);
+        var note_div = domConstruct.create("div", {innerHTML: note['text'], class:note['type']+'2'}, e);
+        if (note['type'] == 'attachment') {
+          var note_div2 = domConstruct.create("div", {}, note_div);
+
+          var attachments = note['_attachments'];
+          var keys = Object.keys(attachments);
+          array.forEach(keys, function(key) {
+            var ioq = {
+              attachment_id: note._id,
+              doc_id: key
+            };
+
+            var ul = domConstruct.create("ul", {}, note_div2);
+            var li = domConstruct.create("li", {}, ul);
+
+            domConstruct.create("a", {href:tag_and_note_config.download_attachment_url + '?' + ioQuery.objectToQuery(ioq), innerHTML: key}, li);
+          });
+        }
+      }
+    }
+
+
+    /// TODO: need to fix this deleteTagHelper so it
+    function deleteTagHelper(evt, tag_and_note_config) {
+      var tag_elem = evt.target.parentElement;
+      var inner_text = tag_elem.textContent;
+      inner_text = inner_text.replace(' (X)','');
+      var visiting_group_id = tag_elem.attributes['hollyrosa:vgid'].value;
+      tags.deleteTag(tag_and_note_config.delete_tag_url, visiting_group_id, tag_elem.parentElement.id, inner_text);
+    }
+
+
+    var tag_dialog; //= tags.createAddTagDialog(tag_and_note_config.add_tag_url, tags.updateTagsCloseDialog );
+
+
+    function show_add_tag_dialog(node_id) {
+      var visiting_group_id = node_id.attributes['hollyrosa:vgid'].value;
+      tags.showAddTagDialog(tag_dialog, visiting_group_id, node_id.attributes['hollyrosa:taglist_node_id'].value);
+    }
+
+
+    function loadNotesFor(id, tag_and_note_config) {
+      xhr(tag_and_note_config.get_notes_for_visiting_group_url, {
+        query: {id: id},
+        handleAs: 'json',
+        method: "GET"
+      }).then( updateNotes );
+    }
+
+    function setup(tag_and_note_config) {
+      on(win.doc, '.tag:click', function(evt) { deleteTagHelper(evt, tag_and_note_config)});
+
+      var tag_ul_elems = query('.tag_list');
+
+      array.forEach(tag_ul_elems, function(ul_tag_elem) {
+        var node_id = ul_tag_elem.id;
+        var vgid = ul_tag_elem.attributes['hollyrosa:vgid'].value;
+        tags.getTags(tag_and_note_config.get_tags_url, vgid, node_id ) ;
+      });
+
+      tag_dialog = tags.createAddTagDialog(tag_and_note_config.add_tag_url, tags.updateTagsCloseDialog );
+    }
+
+    return {
+      show_add_tag_dialog: show_add_tag_dialog,
+      loadNotesFor: loadNotesFor,
+      setup: setup,
+      tag_dialog: tag_dialog
+    }
+  }
+);
---
+++ hollyrosa/public/scripts/page/visiting_group_view_all.js
@@ -0,0 +1,167 @@
+/**
+* Copyright 2010-2017 Martin Eliasson
+*
+* This file is part of Hollyrosa
+*
+* Hollyrosa is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Hollyrosa is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Hollyrosa.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+/**
+Modules that can be loaded:
+
+**/
+define(["common_menu", "page/tag_and_note", "dojo/dom-attr", "dojo/_base/array", "dojo/dom-construct", "dojo/_base/window", "dijit/Menu","dijit/MenuItem", "dijit/PopupMenuItem", "dijit/CheckedMenuItem", "dijit/MenuSeparator", "dojo/query", "dojo/io-query", "dojo/json", "dojo/cookie", "dojo/dom-style", "dojo/domReady!"],
+  function(common_menu, pageAndNote, domAttr, array, domConstruct, win, Menu, MenuItem, PopupMenuItem, CheckedMenuItem, MenuSeparator, query, ioQuery, json, cookie, domStyle)
+{
+
+  function setup(page_config) {
+
+
+    var menu = new Menu({
+      targetNodeIds: ["vgroup_listing"],
+      selector: "tr.name_menu",
+      leftClickToOpen: common_menu.load_left_click_menu(),
+      onOpen: function(evt) {
+        var node = menu.currentTarget;
+        var has_notes = node.attributes["hollyrosa:has_notes"].value;
+
+        if (has_notes == 'True') {
+          load_notes_menu_item.disabled = false;
+        } else {
+          load_notes_menu_item.disabled = true;
+        }
+      } // end function
+    });
+
+
+    common_menu.add_visiting_group_menu_item(menu, menu, "New booking request...", page_config.edit_booking_url);
+    common_menu.add_menu_separator(menu);
+    common_menu.add_visiting_group_menu_item(menu, menu, "View...", page_config.show_visiting_group_url);
+    common_menu.add_visiting_group_menu_item(menu, menu, "Edit...", page_config.edit_visiting_group_url);
+    common_menu.add_call_function_menu_item(menu, menu, "Add tags...",  pageAndNote.show_add_tag_dialog  );
+    common_menu.add_visiting_group_add_note_menu_item(menu, menu, "Add note...", page_config.add_note_url);
+    common_menu.add_visiting_group_add_note_menu_item(menu, menu, "Add attachment...", page_config.add_attachment_url);
+    common_menu.add_menu_separator(menu);
+    //common_menu.add_visiting_group_list_bookings_menu_item(menu, menu, "List bookings of group...", '${tg.url('view_bookings_of_name')}');
+
+    common_menu.add_visiting_group_menu_item(menu, menu, "List bookings of group...", page_config.view_bookings_of_visiting_group_id_url);
+
+    var program_layers_menu = new Menu();
+    menu.addChild(new PopupMenuItem({
+      label: "Program Layers ->",
+      popup: program_layers_menu,
+      leftClickToOpen: common_menu.load_left_click_menu()
+    }));
+
+    common_menu.add_visiting_group_menu_item(menu, program_layers_menu, "Printable ...", page_config.layers_printable_url);
+    common_menu.add_visiting_group_menu_item(menu, program_layers_menu, "Live Calendar...", page_config.layers_url);
+    common_menu.add_visiting_group_menu_item(menu, program_layers_menu, "Edit Connected Layers...", page_config.edit_layers_url);
+    common_menu.add_menu_separator(menu);
+    common_menu.add_visiting_group_menu_item(menu, menu, "Ext. Booking Request", page_config.visiting_group_program_request_edit_request);
+
+    var load_notes_menu_item = new MenuItem({
+      label: "Load notes",
+      onClick: function(evt) {
+        var node = this.getParent().currentTarget;
+        var vgroup_id = node.attributes["hollyrosa:vgid"].value;
+        tagAndNote.loadNotesFor(vgroup_id, page_config.tag_and_note_config);
+
+        //...this isn't bulletproof, should be in a callback after the notes have loaded.
+        var node = menu.currentTarget;
+        node.attributes["hollyrosa:has_notes"].value = "False";
+      }
+    });
+    menu.addChild(load_notes_menu_item);
+
+    common_menu.add_menu_separator(menu);
+    common_menu.add_visiting_group_menu_item(menu, menu, "Show history...", page_config.show_history_url);
+
+    // assembling the filter booking status menu
+    var filter_menu = new Menu({
+      targetNodeIds: ["bokn_status_menu"],
+      leftClickToOpen: common_menu.load_left_click_menu()
+    });
+
+    common_menu.add_redirect_menu_item(filter_menu, filter_menu, "All visiting groups", '', page_config.visiting_groups_view_all_url);
+    common_menu.add_redirect_menu_item(filter_menu, filter_menu, "All visiting groups today", '', page_config.visiting_groups_view_today_url);
+
+    common_menu.add_menu_separator(filter_menu);
+
+    var bokn_status_menu = new Menu();
+    filter_menu.addChild(new PopupMenuItem({
+      label: "Select on Program status...",
+      popup: bokn_status_menu,
+      leftClickToOpen: common_menu.load_left_click_menu()
+    }));
+
+    var vodb_state_menu = new Menu();
+    filter_menu.addChild(new PopupMenuItem({
+      label: "Select on VODB status...",
+      popup: vodb_state_menu,
+      leftClickToOpen: common_menu.load_left_click_menu()
+    }));
+
+    var tag_menu = new Menu();
+    filter_menu.addChild(new PopupMenuItem({
+      label: "Select Tags...",
+      popup: tag_menu,
+      leftClickToOpen: common_menu.load_left_click_menu()
+    }));
+
+    var date_range_menu = new Menu();
+    filter_menu.addChild(new PopupMenuItem({
+      label: "Select Date range...",
+      popup: date_range_menu,
+      leftClickToOpen: common_menu.load_left_click_menu()
+    }));
+
+
+    common_menu.add_menu_separator(filter_menu);
+
+    var view_vgt_menu = new Menu();
+    filter_menu.addChild(new PopupMenuItem({
+      label: "Filter on Group Type...",
+      popup: view_vgt_menu,
+      leftClickToOpen: common_menu.load_left_click_menu()
+    }));
+
+    array.forEach(page_config.program_state_map, function(l) {
+      common_menu.add_redirect_menu_item(filter_menu, bokn_status_menu, l[1], {program_state:l[0]}, page_config.view_program_state_url);
+    });
+
+    array.forEach(page_config.vodb_state_map, function(l) {
+      common_menu.add_redirect_menu_item(filter_menu, vodb_state_menu, l[1], {'vodb_state':l[0]}, page_config.view_vodb_state_url);
+    });
+
+
+    array.forEach(tag_and_note_config.all_tags, function(l) {
+      common_menu.add_redirect_menu_item(filter_menu, tag_menu, l, {tag:l}, tag_and_note_config.view_tags_url);
+    });
+    /* assembling the filter tag menu */
+
+
+    var vgtgroups = page_config.visiting_group_type_list;
+    var vgt_status = common_menu.load_ag_checkbox_status();
+
+    array.forEach(vgtgroups, function(a) {
+      common_menu.add_ag_checkbox_menu_item(view_vgt_menu, a[1], a[0], vgt_status, common_menu.update_visiting_group_type_visible_rows);
+    });
+
+    common_menu.update_visiting_group_type_visible_rows(vgt_status);
+
+  }
+  return {
+    setup: setup
+  }
+});
